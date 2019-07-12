2668a8a2ee8815b52943c3fdda981b84ab4d6b42
Selim Cebecioglu <selimcebecioglu@gmail.com>
2019-07-12 17:57:37 -0400

    windows build

--- package/windows/build.ps1
+++ package/windows/build.ps1
@@ -13,13 +13,18 @@ $VERSION = $(Get-Content .\package.json | Out-String | ConvertFrom-JSON).version
 
 &".\node_modules\.bin\pkg.cmd" . --targets node10-win32-x86 --options trace-warnings --out-path .\package\windows\files\x86\
 
-# Copy ffmpeg, ffprobe, nssm binaries 
+cp -r .\dist-www .\package\windows\files\dist-www
+cp .\node_modules\@serialport\bindings\build\Release\bindings.node .\package\windows\files\bindings.node
+
+# Copy ffmpeg, ffprobe, nssm binaries
 # 64 bit
+cp .\node_modules\sqlite3\lib\binding\node-v64-win32-x64\node_sqlite3.node .\package\windows\files\node_sqlite3.node
 cp .\package\windows\x64\nssm.exe .\package\windows\files\x64\nssm.exe
 cp .\node_modules\@bunchtogether\ffmpeg-static\bin\win32\x64\ffmpeg.exe .\package\windows\files\x64\ffmpeg.exe
 cp .\node_modules\@bunchtogether\ffmpeg-static\bin\win32\x64\ffprobe.exe .\package\windows\files\x64\ffprobe.exe
 
 # 32 bit
+cp .\node_modules\sqlite3\lib\binding\node-v64-win32-x32\node_sqlite3.node .\package\windows\files\node_sqlite3.node
 cp .\package\windows\x86\nssm.exe .\package\windows\files\x86\nssm.exe
 cp .\node_modules\@bunchtogether\ffmpeg-static\bin\win32\x32\ffmpeg.exe .\package\windows\files\x86\ffmpeg.exe
 cp .\node_modules\@bunchtogether\ffmpeg-static\bin\win32\x32\ffprobe.exe .\package\windows\files\x86\ffprobe.exe
