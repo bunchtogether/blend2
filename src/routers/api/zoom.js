0b0e205458638d466f10f6ae1decbed59763d3bc
Adam Wang <adamw3494@gmail.com>
2019-07-24 17:56:38 -0400

    Additional routes for zoom room controls

--- src/routers/api/zoom.js
+++ src/routers/api/zoom.js
@@ -37,5 +37,65 @@ module.exports.getZoomRouter = () => {
     }
   });
 
+  router.post('/audio/volume', async (req: express$Request, res: express$Response) => {
+    const volume = req.body.volume;
+    if (!volume) {
+      res.status(400).send('Missing required body parameter "volume"');
+      return;
+    }
+    try {
+      await zoom.setVolume(volume);
+      res.sendStatus(200);
+    } catch (error) {
+      logger.error('Error setting volume for zoom room');
+      logger.errorStack(error);
+      res.status(400).send('Unable to set volume for zoom room');
+    }
+  });
+
+  router.post('/mic/mute', async (req: express$Request, res: express$Response) => {
+    try {
+      await zoom.muteMic();
+      res.sendStatus(200);
+    } catch (error) {
+      logger.error('Error muting zoom room');
+      logger.errorStack(error);
+      res.status(400).send('Unable to mute zoom room');
+    }
+  });
+
+  router.post('/mic/unmute', async (req: express$Request, res: express$Response) => {
+    try {
+      await zoom.unmuteMic();
+      res.sendStatus(200);
+    } catch (error) {
+      logger.error('Error unmuting zoom room');
+      logger.errorStack(error);
+      res.status(400).send('Unable to unmute zoom room');
+    }
+  });
+
+  router.post('/video/enable', async (req: express$Request, res: express$Response) => {
+    try {
+      await zoom.enableVideo();
+      res.sendStatus(200);
+    } catch (error) {
+      logger.error('Error enabling zoom room video');
+      logger.errorStack(error);
+      res.status(400).send('Unable to enable zoom room video');
+    }
+  });
+
+  router.post('/video/disable', async (req: express$Request, res: express$Response) => {
+    try {
+      await zoom.disableVideo();
+      res.sendStatus(200);
+    } catch (error) {
+      logger.error('Error disabling zoom room video');
+      logger.errorStack(error);
+      res.status(400).send('Unable to disable zoom room video');
+    }
+  });
+
   return router;
 };
--- src/zoom/index.js
+++ src/zoom/index.js
@@ -41,7 +41,47 @@ async function leaveMeeting() {
   }
 }
 
+async function setVolume(volume: number) {
+  logger.info(`Setting Zoom room volume to: ${volume}`);
+  if (activeZoom) {
+    await activeZoom.zconfiguration.audio.output({ volume });
+  }
+}
+
+async function muteMic() {
+  logger.info('Muting Zoom room microphone');
+  if (activeZoom) {
+    await activeZoom.zconfiguration.call.microphone({ mute: 'on' });
+  }
+}
+
+async function unmuteMic() {
+  logger.info('Un-muting Zoom room microphone');
+  if (activeZoom) {
+    await activeZoom.zconfiguration.call.microphone({ mute: 'off' });
+  }
+}
+
+async function enableVideo() {
+  logger.info('Enabling video for Zoom room');
+  if (activeZoom) {
+    await activeZoom.zconfiguration.call.camera({ mute: 'off' });
+  }
+}
+
+async function disableVideo() {
+  logger.info('Disabling video for Zoom room');
+  if (activeZoom) {
+    await activeZoom.zconfiguration.call.camera({ mute: 'on' });
+  }
+}
+
 module.exports = {
   joinMeeting,
   leaveMeeting,
+  muteMic,
+  unmuteMic,
+  setVolume,
+  enableVideo,
+  disableVideo,
 };
