8133a2c762da51fefaf95c00dc3726bd5d4e3bde
Robert Bolen <selimcebecioglu@gmail.com>
2019-08-07 13:28:38 -0400

    use picture in picture

---
+++ dist/lib/picture-in-picture.js
@@ -0,0 +1,20 @@
+//      
+
+const os = require('os');
+const logger = require('./logger')('Picture In Picture');
+
+const platform = os.platform();
+
+let setForegroundWindow = (name        ) => logger.warn(`setForegroundWindow is not available on ${platform}, ${name}`);
+let createWindow = (name        ) => logger.warn(`createWindow is not available on ${platform}, ${name}`);
+
+if (platform === 'win32') {
+  const pictureInPicture = require('@bunchtogether/picture-in-picture'); // eslint-disable-line global-require
+  setForegroundWindow = pictureInPicture.setForegroundWindow;
+  createWindow = pictureInPicture.createWindow;
+}
+
+module.exports = {
+  setForegroundWindow,
+  createWindow,
+};
