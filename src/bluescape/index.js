89123f41541e0e3f7a5aaa05b1942a436d5270af
John Wehr <johnwehr@gmail.com>
2019-08-12 16:35:25 -0400

    Persistent Zoom Room connection, simplify window switching

--- src/bluescape/index.js
+++ src/bluescape/index.js
@@ -1,9 +1,7 @@
 // @flow
 
 const find = require('find-process');
-const { setForegroundWindow } = require('../lib/picture-in-picture');
 const logger = require('../lib/logger')('Bluescape Control');
-const { showBandButton } = require('../lib/desktop-window-button');
 
 const resultPromise = find('name', 'tsx_winmaster', true).catch((error) => {
   logger.error('Error while finding process');
@@ -15,19 +13,6 @@ async function isAvailable() {
   return Array.isArray(result) && result.length > 0;
 }
 
-async function focus() {
-  logger.info('Switching to Bluescape');
-  await setForegroundWindow('tsx_winslave');
-  await showBandButton(() => {
-  	setForegroundWindow("chrome").catch((error) => {
-  		logger.error("Could not set Foreground window");
-  		logger.errorStack(error);
-  	})
-  });
-}
-
-
 module.exports = {
   isAvailable,
-  focus,
 };
