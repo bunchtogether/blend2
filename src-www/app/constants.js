b81f39318da8275ef28850046a979b1636295421
Selim Cebecioglu <selimcebecioglu@gmail.com>
2019-08-15 18:21:15 -0400

    nec adapter

--- src-www/app/constants.js
+++ src-www/app/constants.js
@@ -2,3 +2,17 @@
 
 export const TYPE_VIZIO = 'vizio';
 export const TYPE_SAMSUNG = 'samsung';
+export const TYPE_NEC = 'nec';
+
+export const DISPLAYS = [TYPE_SAMSUNG, TYPE_NEC, TYPE_VIZIO];
+
+export const PORT_NAMES = {
+  [TYPE_SAMSUNG]: 'EX-LINK',
+  [TYPE_NEC]: 'RS-232',
+  [TYPE_VIZIO]: null,
+};
+export const DISPLAY_NAMES = {
+  [TYPE_SAMSUNG]: 'Samsung',
+  [TYPE_NEC]: 'NEC',
+  [TYPE_VIZIO]: 'Vizio',
+};
--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,6 @@ module.exports.API_PORT = 61340;
 
 module.exports.TYPE_VIZIO = 'vizio';
 module.exports.TYPE_SAMSUNG = 'samsung';
+module.exports.TYPE_NEC = 'nec';
 
 module.exports.LEVEL_DB_DEVICE = 'DEVICE';
