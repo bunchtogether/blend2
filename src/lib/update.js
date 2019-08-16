e0cab39074045cc804a5a5bd56b8910df46e1444
Adam Wang <adamw3494@gmail.com>
2019-08-16 16:11:48 -0400

    Do not throw

--- src/lib/update.js
+++ src/lib/update.js
@@ -24,7 +24,7 @@ const logger = require('./logger')('Updater');
 
 const triggerUpdate = async ():Promise<void> => {
   logger.error('Triggering update');
-  throw new Error('triggerUpdate not implemented yet');
+  // throw new Error('triggerUpdate not implemented yet');
 };
 
 module.exports = {
